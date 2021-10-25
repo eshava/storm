c9263b72667000de77fba0c602217098ca42dfa3
TinoKrueger <krueger.tino@googlemail.com>
2021-10-25 21:05:04 +0200

    added GetValue method to ObjectMapper class

--- Eshava.Storm/Interfaces/IObjectMapper.cs
+++ Eshava.Storm/Interfaces/IObjectMapper.cs
@@ -9,5 +9,14 @@
 		/// <param name="tableAlias">Single alias or comma separeted list</param>
 		/// <returns></returns>
 		T Map<T>(string tableAlias = null);
+
+		/// <summary>
+		/// Gets the value for the given column name
+		/// </summary>
+		/// <typeparam name="T">Must not be class, custom data types are possible</typeparam>
+		/// <param name="columnName">Column name</param>
+		/// <param name="tableAlias">Single alias</param>
+		/// <returns></returns>
+		T GetValue<T>(string columnName, string tableAlias = null);
 	}
 }
